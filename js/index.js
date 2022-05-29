3b39f40126f2622d43d1f755315f72e7b82fa103
Johan Cortes <johandcortes@gmail.com>
2022-05-29 10:26:08 -0500

    29052022

--- js/index.js
+++ js/index.js
@@ -5,7 +5,9 @@ let $d = document,
       "#" + ("000000" + col.toString(16)).slice(-6),
       "#" + ("000000" + (16777215 - col).toString(16)).slice(-6),
     ];
-  };
+  },
+  com = false,
+  caso = 2;
 
 class Program {
   constructor(pid, nombre, t_codigo, t_data, t_bss, dir_base) {
@@ -32,23 +34,23 @@ const kernel = 1048576,
     SO: ["S.O.", 431592, 207365, 916],
     Notepad: [
       "Notepad",
-      15654,
+      18654,
       8352,
-      150,
+      166,
       "https://icon-library.com/images/notepad-icon-png/notepad-icon-png-16.jpg",
     ],
     Word: [
       "Word",
-      81465,
-      13548,
+      120465,
+      25548,
       276,
       "https://icon-library.com/images/icon-word/icon-word-5.jpg",
     ],
     Excel: [
       "Excel",
-      91776,
-      15000,
-      300,
+      167776,
+      36426,
+      371,
       "https://icon-library.com/images/excel-sheet-icon/excel-sheet-icon-8.jpg",
     ],
     AutoCAD: [
@@ -60,7 +62,7 @@ const kernel = 1048576,
     ],
     Calculadora: [
       "Calculadora",
-      20480,
+      18280,
       303,
       387,
       "https://icon-library.com/images/windows-calculator-icon/windows-calculator-icon-8.jpg",
@@ -93,18 +95,79 @@ const kernel = 1048576,
       dir = 0;
     exe.forEach((e) => {
       if (e.pid >= pid) pid = e.pid + 1;
-      dir += e.memoria;
     });
-    let programa = new Program(
-      pid,
-      p[0],
-      p[1],
-      p[2],
-      p[3],
-      ("000000" + dir.toString(16)).slice(-6).toUpperCase()
-    );
-    programa.resize();
-    exe.push(programa);
+    if (com || exe.length == 0) {
+      if (exe.length > 0)
+        dir =
+          parseInt(exe[exe.length - 1].dir_base, 16) +
+          exe[exe.length - 1].memoria;
+      let programa = new Program(
+        pid,
+        p[0],
+        p[1],
+        p[2],
+        p[3],
+        ("000000" + dir.toString(16)).slice(-6).toUpperCase()
+      );
+      programa.resize();
+      exe.push(programa);
+    } else {
+      let disp =
+          16777215 -
+          (parseInt(exe[exe.length - 1].dir_base, 16) +
+            exe[exe.length - 1].memoria),
+        index = null,
+        cap = encabezado + stack + heap + p[1] + p[2] + p[3];
+      switch (caso) {
+        case 0: //primer caso
+          for (let x = 0; x < exe.length; x++) {
+            if (!exe[x].nombre && exe[x].memoria >= cap) {
+              index = x;
+              break;
+            }
+          }
+          break;
+        case 1: //mejor caso
+          exe.forEach((e) => {
+            if (e.nombre && e.memoria > disp) disp = e.memoria;
+          });
+          exe.forEach((e, i) => {
+            if (!e.nombre && e.memoria >= cap && e.memoria <= disp) {
+              disp = e.memoria;
+              index = i;
+            }
+          });
+          break;
+        case 2: //peor caso
+          exe.forEach((e, i) => {
+            if (!e.nombre && e.memoria >= cap && e.memoria >= disp) {
+              disp = e.memoria;
+              index = i;
+            }
+          });
+          break;
+      }
+      let programa = new Program(pid, p[0], p[1], p[2], p[3]);
+      programa.resize();
+      if (index) {
+        programa.dir_base = exe[index].dir_base;
+        exe[index].dir_base = (
+          parseInt(exe[index].dir_base, 16) + cap
+        ).toString(16).toUpperCase();
+        exe[index].memoria -= cap;
+        exe[index].memoria === 0
+          ? exe.splice(index, 1, programa)
+          : exe.splice(index, 0, programa);
+      } else {
+        programa.dir_base = (
+          parseInt(exe[exe.length - 1].dir_base, 16) +
+          exe[exe.length - 1].memoria
+        )
+          .toString(16)
+          .toUpperCase();
+        exe.push(programa);
+      }
+    }
   };
 
 let drawProc = () => {
@@ -120,15 +183,17 @@ let drawProc = () => {
       <article><span>Dir base</span></article>`;
     exe.forEach((e) => {
       let $articles = ``;
-      for (const key in e) {
-        if (key === "dir_base") {
-          $articles += `<article><span>${
-            parseInt((e.memoria / 1024) * 100) / 100
-          }</span></article>`;
+      if (e.nombre) {
+        for (const key in e) {
+          if (key === "dir_base") {
+            $articles += `<article><span>${
+              parseInt((e.memoria / 1024) * 100) / 100
+            }</span></article>`;
+          }
+          $articles += `<article><span>${e[key]}</span></article>`;
         }
-        $articles += `<article><span>${e[key]}</span></article>`;
+        proc.innerHTML += $articles;
       }
-      proc.innerHTML += $articles;
     });
   },
   drawRam = () => {
@@ -136,11 +201,6 @@ let drawProc = () => {
       16777215 -
       (parseInt(exe[exe.length - 1].dir_base, 16) +
         exe[exe.length - 1].memoria);
-    /* console.log(
-      `16777215 - (${parseInt(exe[exe.length - 1].dir_base, 16)} + ${
-        exe[exe.length - 1].memoria
-      }) = ${libre}`
-    ); */
     $ram = $d.querySelector(".ram");
     $ram.innerHTML = "";
     exe.forEach((e) => {
@@ -148,7 +208,11 @@ let drawProc = () => {
       let $article = $d.createElement("article"),
         $span = $d.createElement("span");
       $span.textContent = e.nombre;
+      $article.dataset.pid = e.pid;
+      $article.dataset.dir = e.dir_base;
+      $article.dataset.memoria = e.memoria;
       $article.appendChild($span);
+      if (!e.nombre) col[0] = col[1] = "#ddd";
       $article.setAttribute(
         "style",
         `background: ${col[0]};text-shadow: -1.5px 2.5px 2px black, 1.5px -1.5px 0.08em ${col[1]}, 0 0 2em black;flex-grow: ${e.memoria}`
@@ -183,10 +247,16 @@ let drawProc = () => {
         let programa = programas[$figure.children[1].children[0].textContent],
           capacidad =
             encabezado + stack + heap + programa[1] + programa[2] + programa[3];
-        if (capacidad <= libre) {
+        let nones = exe.filter((e) => !e.nombre),
+          disp = false;
+        nones.forEach((e) => {
+          if (capacidad <= e.memoria) disp = true;
+        });
+        if (capacidad <= libre || disp) {
           lanzar(programa);
           drawProc();
           drawRam();
+          console.log(exe);
         } else {
           alert("No hay memoria suficiente");
         }
@@ -199,22 +269,36 @@ let drawProc = () => {
       while (!$article.matches("article")) $article = $article.parentNode;
       let index = exe.indexOf(exe[segs.indexOf($article)]),
         memoria = exe[index].memoria;
-      console.log(index, exe[index]);
-      if (index) {
-        exe.splice(index, 1);
-        exe.forEach((e, i) => {
-          if (i >= index)
-            e.dir_base = (parseInt(e.dir_base, 16) - memoria)
-              .toString(16)
-              .toUpperCase();
-        });
+      if (index && exe[index].pid) {
+        if (com) {
+          exe.splice(index, 1);
+          exe.forEach((e, i) => {
+            if (i >= index)
+              e.dir_base = (parseInt(e.dir_base, 16) - memoria)
+                .toString(16)
+                .toUpperCase();
+          });
+        } else {
+          let none = new Program();
+          none.memoria = memoria;
+          none.dir_base = exe[index].dir_base;
+          index === exe.length - 1 ? exe.pop() : (exe[index] = none);
+          for (let x = 1; x < exe.length; x++) {
+            if (!exe[x - 1].nombre && !exe[x].nombre) {
+              console.log(exe[x - 1], exe[x]);
+              exe[x - 1].memoria += exe[x].memoria;
+              exe.splice(x, 1);
+              x--;
+            }
+          }
+          if (!exe[exe.length - 1].nombre) exe.pop();
+          console.log(exe);
+        }
         drawProc();
         drawRam();
       }
     });
   };
-
-console.log(programas);
 
 (() => {
   lanzar(programas.SO);
